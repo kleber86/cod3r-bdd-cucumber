8ea4257b54170c8f69ab6914c5f20b62126ae422
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-04-23 23:36:27 -0300

    CucumberOptions

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,6 +1,3 @@
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
@@ -9,17 +6,13 @@ public class AprenderCucumber {
 
     @Dado("que criei o arquivo corretamente")
     public void que_criei_o_arquivo_corretamente() {
-
     }
 
     @Quando("executa-lo")
     public void executa_lo() {
-
     }
 
     @Então("a especificação deve finalizar com sucesso")
     public void a_especificação_deve_finalizar_com_sucesso() {
-
     }
-
 }
