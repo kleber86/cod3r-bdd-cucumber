45278e5f00600601df43a970c0cea422e68c346c
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-04-25 20:18:27 -0300

    Reaproveitando cenários - REGEX

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -2,6 +2,10 @@ import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
@@ -36,4 +40,35 @@ public class AprenderCucumber {
     public void oValorDoContadorÉ(Integer int1) {
        assertEquals(int1, contador);
     }
+
+    Date entrega = new Date();
+
+    @Dado("que a entrega é dia {int}\\/{int}\\/{int}")
+    public void queAEntregaÉDia(Integer dia, Integer mes, Integer ano) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.DAY_OF_MONTH, dia);
+        cal.set(Calendar.MONTH, mes - 1);
+        cal.set(Calendar.YEAR, ano);
+        entrega = cal.getTime();
+    }
+    @Quando("a entrega atrasar em (\\d+) (.+)$")
+    public void aEntregaAtrasarEmDias(Integer int1, String tempo) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(entrega);
+        if(tempo.equals("dias")){
+            cal.add(Calendar.DAY_OF_MONTH, int1);
+        }
+        if(tempo.equals("meses")){
+            cal.add(Calendar.MONTH, int1);
+        }
+        entrega = cal.getTime();
+    }
+    @Então("a entrefa será efetuada em (\\d{2}\\/\\d{2}/\\d{4})$")
+    public void aEntrefaSeráEfetuadaEm(String data) {
+        DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        String dataFormatada = format.format(entrega);
+        assertEquals(data, dataFormatada);
+    }
+
+
 }
--- src/test/java/aprender_cucumber.feature
+++ src/test/java/aprender_cucumber.feature
@@ -15,3 +15,13 @@ Cenario: Deve incrementar contador
   Dado que o valor do contador é 15
   Quando eu incrementar em 3
   Então o valor do contador é 18
+
+Cenario: Deve calcular atraso na entrega
+  Dado que a entrega é dia 25/04/2022
+  Quando a entrega atrasar em 2 dias
+  Então a entrefa será efetuada em 27/04/2022
+
+Cenario: Deve calcular atraso na entrega da china
+    Dado que a entrega é dia 25/04/2022
+    Quando a entrega atrasar em 2 meses
+    Então a entrefa será efetuada em 25/06/2022
