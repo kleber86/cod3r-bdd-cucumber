a8f2ea4eb369f638182742ea3b8b2559125e1fd7
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-04-30 17:19:42 -0300

    Montando o ambiente de testes Web

---
+++ src/main/java/br/estudos/TesteAmbiente.java
@@ -0,0 +1,11 @@
+package br.estudos;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteAmbiente {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://seubarriga.wcaquino.me/");
+    }
+}
